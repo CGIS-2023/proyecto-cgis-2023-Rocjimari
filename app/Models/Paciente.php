81310786f4b88b2ce98131320b35d594923cc7d1
Rocjimari <rocjimari@alum.us.es>
2023-03-18 16:37:21 +0100

    Paciente

---
+++ app/Models/Paciente.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Paciente extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nombre',
+        'sexo',
+        'edad',
+        'estado',
+    ];
+    
+
+    protected $hidden =[
+        'id'
+    ];
+
+    protected $casts = [
+        'edad' => 'integer',
+    ];
+    
+    // //Listado de los pacientes de la tabla
+    // public function ObtenerPacientes(){
+    //     return Paciente::all();
+    // }
+    // //Listado de los pacientes de la tabla por Id
+    // public function ObtenerPacientesPorId($id){
+    //     return Paciente::find($id);
+    // }
+
+/////////////////////////////////////////////////////
+
+
+    public function cama(){
+        return $this->hasOne(Cama::class);
+    }
+
+    public function equipamientos(){
+        return $this->hasMany(Equipamiento::class);
+    }
+
+    public function Area_UCI(){
+        return $this->belongTo(Area_UCI::class);
+    }
+    
+    public function enfermeros(){
+        return $this->belongsToMany(Enfermero::class)->withPivot('inicio','fin','notas','estado');
+    }
+
+    public function enfermedads(){
+        return $this->belongsToMany(Enfermedad::class)->withPivot('');
+    }
+    
+
+
+
+}
