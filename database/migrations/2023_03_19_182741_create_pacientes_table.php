a766e38eddffa981229014ec58bc4ee13b344a84
Rocjimari <rocjimari@alum.us.es>
2023-03-19 20:11:46 +0100

    tabla Pacientes

---
+++ database/migrations/2023_03_19_182741_create_pacientes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   
+    public function up(): void
+    {
+        Schema::create('pacientes', function(Blueprint $table){
+            $table->string('nombre');
+            $table->string('sexo');
+            $table->integer('edad');;
+            $table->string('estado');
+            $table->date('fecha_entrada');
+            $table->date('fecha_salida');            
+            $table->string('estado_inicial');
+            $table->id();
+            $table->timestamps();
+  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};
