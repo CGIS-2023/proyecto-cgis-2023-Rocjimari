234f14f363d516e7d3bdc984ca4e2471b986b8a4
Rocjimari <rocjimari@alum.us.es>
2023-03-19 21:47:04 +0100

    Editar Paciente

---
+++ resources/views/pacientes/edit.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar Paciente {{$paciente-> nombre}}</title>
+</head>
+<body>
+Editar Paciente: {{$paciente-> nombre}}
+<br>
+<form action="{{route('pacientes.update', $paciente->id)}}" method="POST" role="form">
+        {{ csrf_field()}} 
+        @method('put')
+        <br>
+        <div class="py-12">
+            <label for="">Nombre</label>
+            <input type="text" name="nombre" value="{{$paciente-> nombre}}">
+        </div>
+
+        <div class="py-12">
+            <label for="">Edad</label>
+            <input type="number" name="edad" id="edad" min="0" value="{{$paciente-> edad}}">
+        </div>        
+        <div class="py-12">
+            <label for="sexo">Sexo</label>
+            <select name="sexo" id="sexo">
+                <option value="mujer" {{($paciente->sexo == 'mujer')? 'selected': ''}}>Mujer</option>
+                <option value="hombre" {{($paciente->sexo == 'hombre')? 'selected': ''}}>Hombre</option>
+            </select>
+        </div>
+        <div class="py-12">
+            <label for="estado">Estado</label>
+            <select name="estado" id="estado">
+                <option value="vivo" {{($paciente->estado == 'vivo')? 'selected': ''}}>Vivo</option>
+                <option value="muerto"{{($paciente->estado == 'muerto')? 'selected': ''}}>Muerto</option>
+            </select>
+            
+        </div>
+        <div class="py-12">
+            <label for="fecha_entrada">Fecha entrada</label>
+            <input type="datetime-local" name="fecha_entrada" value = "{{$paciente->fecha_entrada->format('Y-m-d\TH:i:s')}}">
+        </div>
+        
+        <div class="py-12">
+            <label for="fecha_salida">Fecha salida</label>
+            <input type="datetime-local" name="fecha_entrada" value = "{{$paciente->fecha_salida->format('Y-m-d\TH:i:s')}}" >
+        </div>
+
+        <div class="py-12">
+            <label for="estado_inicial">Estado inicial</label>
+            <select name="estado_inicial" id="estado_inicial">                
+                <option value="Grave" {{($paciente->estado_inicial == 'Grave')? 'selected' : ''}}>Grave</option>
+                <option value="Agudo"{{($paciente->estado_inicial == 'Agudo')? 'selected' : ''}}>Agudo</option>                
+                <option value="Potencialmente recuperable"{{($paciente->estado_inicial == 'Potencialmente recuperable')? 'selected' : ''}}>Potencialmente recuperable</option>
+            </select>
+            
+        </div>
+        <br>
+        <button type="submit" class="btn btn-danger btn-sm">Editar</button>
+        
+    </form>
+</body>
+</html>
