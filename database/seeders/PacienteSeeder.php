3bb9f0e0914ecab7ebec2be55b90b73ab5662bae
Rocjimari <rocjimari@alum.us.es>
2023-03-19 22:28:08 +0100

    Paciente seeder

--- database/seeders/PacienteSeeder.php
+++ database/seeders/PacienteSeeder.php
@@ -17,7 +17,8 @@ class PacienteSeeder extends Seeder
     {
         DB::table('pacientes')->insert([
             [
-               'nombre' => "Juan",                
+               'nombre' => "Juan",
+               'apellidos' => 'López Tolosa' ,               
                'sexo' => 'Hombre',
                'edad' => 24,
                'fecha_entrada' => '2021-05-30 10:15:00',               
@@ -27,7 +28,8 @@ class PacienteSeeder extends Seeder
 
            ],
            [
-            'nombre' => "Isabel",                
+            'nombre' => "Isabel", 
+            'apellidos' => 'Velázquez Dominguez',               
             'sexo' => 'Mujer',
             'edad' => 67,
             'fecha_entrada' => '2021-05-30 10:15:00',               
@@ -35,7 +37,29 @@ class PacienteSeeder extends Seeder
             'estado' => 'Vivo',
             'estado_salud_inicial' => 'Agudo'
 
-        ],
+            ],
+            [
+            'nombre' => "Marisol",   
+            'apellido' => 'Campos Trigos',            
+            'sexo' => 'Mujer',
+            'edad' => 34,
+            'fecha_entrada' => '2021-05-30 10:15:00',               
+            'fecha_salida' => '2021-05-30 10:15:00',
+            'estado' => 'Muerto',
+            'estado_salud_inicial' => 'Grave'
+
+            ],
+            [
+            'nombre' => "Antonio",
+            'apellidos' => "Cuevas Carrión",                
+            'sexo' => 'Hombre',
+            'edad' => 56,
+            'fecha_entrada' => '2021-05-30 10:15:00',               
+            'fecha_salida' => '2021-05-30 10:15:00',
+            'estado' => 'Muerto',
+            'estado_salud_inicial' => 'Potencialmente recuperable'
+    
+                ],
         ]);
     }
 }
