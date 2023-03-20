4eb636ca75ffda8470e11b2ff058fe0157dd41a2
Rocjimari <rocjimari@alum.us.es>
2023-03-20 15:15:56 +0100

    Pagina privada

---
+++ resources/views/secret.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página privada</title>
+</head>
+<body>
+    
+    <div>
+        <header>
+            <a >Pagina privada</a>
+        <div>            
+            <article class="container">
+                <h2>Sección privada:</h2>
+                
+            </article>
+            <a href="{{route('logout')}}"><button type="button">Salir</button></a>
+        </div>
+        </header>
+    </div>
+    
+</body>
+</html>
