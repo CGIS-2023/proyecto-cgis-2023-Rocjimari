f679e46685597d4c7832c978979be38d9b420aa1
Rocjimari <rocjimari@alum.us.es>
2023-03-22 16:23:51 +0100

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,80 +1,47 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" 
-    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" 
-    crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('csss/app.css')}}">
-</head>
-</head>
-<body>
-    <div class="container d-flex">
-        <form action="{{route('login.verify')}}" method="POST" class="m-auto bg-white p-5 rounded-sm shadow-lg w-form">
-            @csrf
-            <h2 class="text-center">
-                Login
-            </h2>
-            @error('invalid_credentials')
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    <small class="text-danger mt-1">
-                        {{$message}}
-                    </small>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                    
-            @enderror
-            @if(session('mensaje'))
-                <div class="alert alert-success">
-                    {{session('mensaje')}}
-                </div>
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
             @endif
 
-            @if (session('success'))
-                <div class="alert alert-success" role="alert">
-                    <small >
-                        {{session('success')}}
-                    </small>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            @endif
-            <div class="form-group">
-                <label for="inputEmail">Email</label>
-                <input type="email" name = "email" value="{{ old('email') }}" id="inputEmail" class="form-control" 
-                aria-describedby="emailHelp" placeholder="Enter email">
-                @error('email')
-                    <small class="text-danger mt-1">
-                        <strong>{{$message}}</strong>
-                    </small>
-                @enderror
-            </div>
-            <div class="form-group">
-                <label for="inputPassword">Password</label>
-                <input type="password" name = "password" id="inputPassword" class="form-control" placeholder="Password">
-                @error('password')
-                    <small class="text-danger mt-1">
-                        <strong>{{$message}}</strong>
-                    </small>
-                @enderror
-            </div>
-            <button type="submit" class="btn btn-primary btn-block">Login</button>
-            <div><a href="{{route('register')}}">Registrarme</a></div>
-        </form>
-
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
-    </script>
-    
-</body>
-</html>
+            <x-primary-button class="ml-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
